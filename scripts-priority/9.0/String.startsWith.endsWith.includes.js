d91daf075bdf0d882bff1cd8f73e9d3fb06957b2
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-07-08 22:27:51 +0700

    Fix String.startsWith, String.endsWith, Document Fullscreen API polyfills

--- scripts-priority/9.0/String.startsWith.endsWith.includes.js
+++ scripts-priority/9.0/String.startsWith.endsWith.includes.js
@@ -2,53 +2,59 @@ var $toString = Object.prototype.toString;
 var $indexOf = String.prototype.indexOf;
 var $lastIndexOf = String.prototype.lastIndexOf;
 
-// http://people.mozilla.org/~jorendorff/es6-draft.html#sec-string.prototype.startswith
-export function startsWith(search) {
-    /*! http://mths.be/startswith v0.1.0 by @mathias */
-    var string = String(this);
-    if (this == null || $toString.call(search) == '[object RegExp]') {
-        throw TypeError();
+if (!String.prototype.startsWith) {
+    // http://people.mozilla.org/~jorendorff/es6-draft.html#sec-string.prototype.startswith
+    function startsWith(search) {
+        /*! http://mths.be/startswith v0.1.0 by @mathias */
+        var string = String(this);
+        if (this == null || $toString.call(search) == '[object RegExp]') {
+            throw TypeError();
+        }
+        var stringLength = string.length;
+        var searchString = String(search);
+        var searchLength = searchString.length;
+        var position = arguments.length > 1 ? arguments[1] : undefined;
+        // `ToInteger`
+        var pos = position ? Number(position) : 0;
+        if (isNaN(pos)) {
+            pos = 0;
+        }
+        var start = Math.min(Math.max(pos, 0), stringLength);
+        return $indexOf.call(string, searchString, pos) == start;
     }
-    var stringLength = string.length;
-    var searchString = String(search);
-    var searchLength = searchString.length;
-    var position = arguments.length > 1 ? arguments[1] : undefined;
-    // `ToInteger`
-    var pos = position ? Number(position) : 0;
-    if (isNaN(pos)) {
-        pos = 0;
-    }
-    var start = Math.min(Math.max(pos, 0), stringLength);
-    return $indexOf.call(string, searchString, pos) == start;
+    String.prototype.startsWith = startsWith;
 }
 
-// http://people.mozilla.org/~jorendorff/es6-draft.html#sec-string.prototype.endswith
-export function endsWith(search) {
-    /*! http://mths.be/endswith v0.1.0 by @mathias */
-    var string = String(this);
-    if (this == null || $toString.call(search) == '[object RegExp]') {
-        throw TypeError();
-    }
-    var stringLength = string.length;
-    var searchString = String(search);
-    var searchLength = searchString.length;
-    var pos = stringLength;
-    if (arguments.length > 1) {
-        var position = arguments[1];
-        if (position !== undefined) {
-            // `ToInteger`
-            pos = position ? Number(position) : 0;
-            if (isNaN(pos)) {
-                pos = 0;
+if (!String.prototype.endsWith) {
+    // http://people.mozilla.org/~jorendorff/es6-draft.html#sec-string.prototype.endswith
+    function endsWith(search) {
+        /*! http://mths.be/endswith v0.1.0 by @mathias */
+        var string = String(this);
+        if (this == null || $toString.call(search) == '[object RegExp]') {
+            throw TypeError();
+        }
+        var stringLength = string.length;
+        var searchString = String(search);
+        var searchLength = searchString.length;
+        var pos = stringLength;
+        if (arguments.length > 1) {
+            var position = arguments[1];
+            if (position !== undefined) {
+                // `ToInteger`
+                pos = position ? Number(position) : 0;
+                if (isNaN(pos)) {
+                    pos = 0;
+                }
             }
         }
+        var end = Math.min(Math.max(pos, 0), stringLength);
+        var start = end - searchLength;
+        if (start < 0) {
+            return false;
+        }
+        return $lastIndexOf.call(string, searchString, start) == start;
     }
-    var end = Math.min(Math.max(pos, 0), stringLength);
-    var start = end - searchLength;
-    if (start < 0) {
-        return false;
-    }
-    return $lastIndexOf.call(string, searchString, start) == start;
+    String.prototype.endsWith = endsWith;
 }
 
 // https://github.com/alfaslash/string-includes-polyfill/blob/master/string-includes-polyfill.js
--- scripts/16.4/document.fullscreen.js
+++ scripts/16.4/document.fullscreen.js
@@ -1,169 +1,128 @@
 // https://github.com/nguyenj/fullscreen-polyfill
-// Modified with Claude
-const spec = [
-    "fullscreen",
-    "fullscreenEnabled",
-    "fullscreenElement",
-    "fullscreenchange",
-    "fullscreenerror",
-    "exitFullscreen",
-    "requestFullscreen",
-];
+(function () {
+    "use strict";
 
-const webkit = [
-    "webkitIsFullScreen",
-    "webkitFullscreenEnabled",
-    "webkitFullscreenElement",
-    "webkitfullscreenchange",
-    "webkitfullscreenerror",
-    "webkitExitFullscreen",
-    "webkitRequestFullscreen",
-];
+    const spec = [
+        "fullscreen",
+        "fullscreenEnabled",
+        "fullscreenElement",
+        "fullscreenchange",
+        "fullscreenerror",
+        "exitFullscreen",
+        "requestFullscreen",
+    ];
 
-// Make sure document exist, if it doesn't make it a dumb object
-if (typeof document === "undefined") {
-    try {
-        document = {};
-    } catch (e) {
-        // Cannot create document in this environment
-    }
-}
+    const webkit = [
+        "webkitIsFullScreen",
+        "webkitFullscreenEnabled",
+        "webkitFullscreenElement",
+        "webkitfullscreenchange",
+        "webkitfullscreenerror",
+        "webkitExitFullscreen",
+        "webkitRequestFullscreen",
+    ];
 
-// Get the vendor fullscreen prefixed api
-const fsVendorKeywords = (function getFullscreenApi() {
-    // Safety check for document
-    if (!document || typeof document !== "object") {
-        return [];
-    }
+    // Get the vendor fullscreen prefixed api
+    const fsVendorKeywords = (function getFullscreenApi() {
+        const fullscreenEnabled = [spec[1], webkit[1]].find(
+            (prefix) => prefix && document[prefix]
+        );
+        return (
+            [spec, webkit].find((vendor) => {
+                return vendor.find((prefix) => prefix === fullscreenEnabled);
+            }) || []
+        );
+    })();
 
-    const fullscreenEnabled = [spec[1], webkit[1], moz[1], ms[1]].find(
-        (prefix) => prefix && document[prefix]
-    );
-    return (
-        [spec, webkit, moz, ms].find((vendor) => {
-            return vendor.find((prefix) => prefix === fullscreenEnabled);
-        }) || []
-    );
-})();
+    function handleEvent(eventType, event) {
+        try {
+            document[spec[0]] =
+                document[fsVendorKeywords[0]] ||
+                !!document[fsVendorKeywords[2]] ||
+                false;
+            document[spec[1]] = document[fsVendorKeywords[1]] || false;
+            document[spec[2]] = document[fsVendorKeywords[2]] || null;
 
-function handleEvent(eventType, event) {
-    try {
-        document[spec[0]] =
-            document[fsVendorKeywords[0]] ||
-            !!document[fsVendorKeywords[2]] ||
-            false;
-        document[spec[1]] = document[fsVendorKeywords[1]] || false;
-        document[spec[2]] = document[fsVendorKeywords[2]] || null;
-
-        if (typeof document.dispatchEvent === "function") {
             document.dispatchEvent(new Event(eventType));
-        }
-    } catch (e) {
-        // Handle event failed, but don't break the page
-        if (typeof console !== "undefined" && console.warn) {
+        } catch (e) {
             console.warn("Fullscreen event handling failed:", e);
         }
     }
-}
 
-function setupShim() {
-    // Don't setup if we don't have valid vendor keywords
-    if (!fsVendorKeywords.length) {
-        return;
-    }
-
-    try {
-        // fullscreen
-        // Defaults to false for cases like MS where they do not have this
-        // attribute. Another way to check whether fullscreen is active is to look
-        // at the fullscreenElement attribute.
-        document[spec[0]] =
-            document[fsVendorKeywords[0]] ||
-            !!document[fsVendorKeywords[2]] ||
-            false;
-
-        // fullscreenEnabled
-        document[spec[1]] = document[fsVendorKeywords[1]] || false;
-
-        // fullscreenElement
-        document[spec[2]] = document[fsVendorKeywords[2]] || null;
-
-        // onfullscreenchange
-        if (
-            fsVendorKeywords[3] &&
-            typeof document.addEventListener === "function"
-        ) {
-            document.addEventListener(
-                fsVendorKeywords[3],
-                handleEvent.bind(document, spec[3]),
-                false
-            );
+    function setupShim() {
+        // Don't setup if we don't have valid vendor keywords
+        if (!fsVendorKeywords.length) {
+            return;
         }
 
-        // onfullscreenerror
-        if (
-            fsVendorKeywords[4] &&
-            typeof document.addEventListener === "function"
-        ) {
-            document.addEventListener(
-                fsVendorKeywords[4],
-                handleEvent.bind(document, spec[4]),
-                false
-            );
-        }
+        try {
+            // fullscreen
+            // Defaults to false for cases like MS where they do not have this
+            // attribute. Another way to check whether fullscreen is active is to look
+            // at the fullscreenElement attribute.
+            document[spec[0]] =
+                document[fsVendorKeywords[0]] ||
+                !!document[fsVendorKeywords[2]] ||
+                false;
 
-        // exitFullscreen
-        if (typeof document[fsVendorKeywords[5]] === "function") {
-            document[spec[5]] = function () {
-                return document[fsVendorKeywords[5]]();
-            };
-        }
+            // fullscreenEnabled
+            document[spec[1]] = document[fsVendorKeywords[1]] || false;
 
-        // requestFullscreen
-        if (
-            typeof Element !== "undefined" &&
-            Element.prototype &&
-            fsVendorKeywords[6]
-        ) {
-            Element.prototype[spec[6]] = function () {
-                if (typeof this[fsVendorKeywords[6]] === "function") {
-                    return this[fsVendorKeywords[6]].apply(this, arguments);
-                }
-            };
-        }
-    } catch (e) {
-        // Polyfill setup failed, but don't break the page
-        if (typeof console !== "undefined" && console.warn) {
+            // fullscreenElement
+            document[spec[2]] = document[fsVendorKeywords[2]] || null;
+
+            // onfullscreenchange
+            if (fsVendorKeywords[3]) {
+                document.addEventListener(
+                    fsVendorKeywords[3],
+                    handleEvent.bind(document, spec[3]),
+                    false
+                );
+            }
+
+            // onfullscreenerror
+            if (fsVendorKeywords[4]) {
+                document.addEventListener(
+                    fsVendorKeywords[4],
+                    handleEvent.bind(document, spec[4]),
+                    false
+                );
+            }
+
+            // exitFullscreen
+            if (typeof document[fsVendorKeywords[5]] === "function") {
+                document[spec[5]] = function () {
+                    return document[fsVendorKeywords[5]]();
+                };
+            }
+
+            // requestFullscreen
+            if (Element.prototype && fsVendorKeywords[6]) {
+                Element.prototype[spec[6]] = function () {
+                    if (typeof this[fsVendorKeywords[6]] === "function") {
+                        return this[fsVendorKeywords[6]].apply(this, arguments);
+                    }
+                };
+            }
+        } catch (e) {
             console.warn("Fullscreen polyfill setup failed:", e);
         }
     }
-}
 
-// Don't polyfill if it already exist or use UMD pattern for better compatibility
-(function () {
-    const shouldSetup =
-        typeof document !== "undefined" &&
-        typeof document[spec[1]] === "undefined" &&
-        fsVendorKeywords.length > 0;
+    // Don't polyfill if it already exists
+    function initPolyfill() {
+        const shouldSetup =
+            typeof document[spec[1]] === "undefined" &&
+            fsVendorKeywords.length > 0;
 
-    let polyfillResult = {};
-
-    if (shouldSetup) {
-        setupShim();
-        polyfillResult = {
-            setupShim: setupShim,
-            isPolyfilled: true,
-        };
+        if (shouldSetup) {
+            setupShim();
+        }
     }
 
-    // Export for different module systems
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = polyfillResult;
-    } else if (typeof define === "function" && define.amd) {
-        define(function () {
-            return polyfillResult;
-        });
-    } else if (typeof window !== "undefined") {
-        window.fullscreenPolyfill = polyfillResult;
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", initPolyfill);
+    } else {
+        initPolyfill();
     }
 })();
