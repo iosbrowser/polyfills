16163cfcfbb50dee646b25d11c9831ebf7b72591
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-07-10 23:09:23 +0700

    Fix viewport CSS workaround

--- scripts-post/15.4/body-overflow-width.js
+++ scripts-post/15.4/body-overflow-width.js
@@ -1,9 +1,10 @@
 (function () {
-    if (window.innerWidth <= 480 && !document.getElementById('pl-body-html')) {
-        const styleSheet = document.createElement('style');
-        styleSheet.type = 'text/css';
-        styleSheet.innerText = `body,html{overflow-x:hidden;}`;
-        styleSheet.id = 'pl-body-html';
+    const styleSheetId = "skip-polyfill-pl-body-html";
+    if (window.innerWidth <= 480 && !document.getElementById(styleSheetId)) {
+        const styleSheet = document.createElement("style");
+        styleSheet.type = "text/css";
+        styleSheet.innerText = `header{width:100%;max-width:100vw;overflow-x:hidden;}.flex-1{min-width:0;}`;
+        styleSheet.id = styleSheetId;
         document.head.appendChild(styleSheet);
     }
 })();
